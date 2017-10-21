9072af8e86077f966f7b07b344a24f89c8cc3a96
Kirill-Al <kirillamsv@gmail.com>
2017-10-22 04:26:49 +1000

    second commit

--- Task2_var3_2.py
+++ Task2_var3_2.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-def is_prime(number):
-    for i in range(3,number//2,2):
-        if number%i == 0:
-            return False
-    else:
+'''Написать функцию is_prime, принимающую 1 аргумент — число от 0 до 1000, и возвращающую True, если оно простое, и False - иначе.'''
+def is_prime(n, div=3):
+    if (n%2 == 0) or (n%div==0) or (n == 1) or (n == 0):
+        return False
+    elif div>=n//2:
         return True
+    else:
+        return is_prime(n, div+2)
 
-number = int(input('Введите число от 0 до 1000: '))
-if 0<=number<=1000:
-    print(is_prime(number))
+n = int(input('Введите число от 0 до 1000: '))
+if 0<=n<=1000:
+    print(is_prime(n))
 else:
     print('Необходимо число от 0 до 1000!')
